8400923600c114d6961603ff4e5a8589eddc43fd
matkar27 <78582561+matkar27@users.noreply.github.com>
2021-02-28 15:47:49 +0530

    Step 3- Using the reverse greyscale colormap

--- Classwork/Image_processing_codes.py
+++ Classwork/Image_processing_codes.py
@@ -12,3 +12,14 @@ plt.show()
 
 brain_image.shape
 plt.show()
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+brain_image=plt.imread("Brain.jpg") 
+plt.imshow(brain_image)
+plt.title('Display Image read using imread()') 
+plt.axis('off')
+plt.show()
+plt.imshow(brain_image, cmap='Greys_r')
+plt.show()
