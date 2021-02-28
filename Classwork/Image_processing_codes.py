07333abbc002ed5dec861beaecc550bc4f9f9f1f
matkar27 <78582561+matkar27@users.noreply.github.com>
2021-02-28 15:36:15 +0530

    Step 1- Reading image

---
+++ Classwork/Image_processing_codes.py
@@ -0,0 +1,11 @@
+# Assignment-1
+### image processing codes
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+brain_image=plt.imread("Brain.jpg") 
+plt.imshow(brain_image)
+plt.title('Display Image read using imread()') 
+plt.axis('off')
+plt.show()
